860a7b4e79f2637feccd3360191d637db5c49e63
Kalgi Malani <67944474+Kalgi-94@users.noreply.github.com>
2024-04-14 14:47:47 -0400

    Update nic.tf

--- nic.tf
+++ nic.tf
@@ -1,12 +1,12 @@
-resource "azurerm_network_interface" "TF-nic" {
-  name                = "azurerm_virtual_network.TF-network.name-nic"
+resource "azurerm_network_interface" "TFnic" {
+  name                = "azurerm_virtual_network.TFnetwork.namenic"
   location            = azurerm_resource_group.TFrg.location
   resource_group_name = azurerm_resource_group.TFrg.name
 
   ip_configuration {
     name                          = "interanl"
-    subnet_id                     = azurerm_subnet.TF-internal.id
+    subnet_id                     = azurerm_subnet.TFinternal.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.TF-public-ip.id
+    public_ip_address_id = azurerm_public_ip.TFpublicip.id
   }
 }
