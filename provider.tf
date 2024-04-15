858dc6e022bc836e73b21099eddda773693ec002
Kalgi Malani <67944474+Kalgi-94@users.noreply.github.com>
2024-04-15 15:47:39 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,6 +11,6 @@ provider "azurerm" {
  subscription_id = "3ec5e19d-8ef4-4c47-a9f2-f2ece03412ed"
   //client_id       = "0f38f3b4-3bc7-440b-811d-fe754b64837d"
   //client_secret   = "c1c514a5-7d76-47a7-a356-0c5ba4d5afbd"
-  //tenant_id       = "50910f11-83a2-4159-bd1b-f0aa1521666e"
+  tenant_id       = "50910f11-83a2-4159-bd1b-f0aa1521666e"
   features {}
 }
