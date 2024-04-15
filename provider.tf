d115b7ed9214a1b67a105bd08e207f6aff491186
Kalgi Malani <67944474+Kalgi-94@users.noreply.github.com>
2024-04-14 23:17:12 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,14 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.99.0"
+      source  = "hashicorp/azurerm"
+      version = "3.94.0"
     }
   }
 }
 
 provider "azurerm" {
-  # Configuration options
+  features {
+  }
+
 }
