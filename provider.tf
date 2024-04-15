1e758f467597b23a20f091dfd535975bd8795d78
Kalgi Malani <67944474+Kalgi-94@users.noreply.github.com>
2024-04-15 16:35:20 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -15,3 +15,10 @@ provider "azurerm" {
 skip_provider_registration = true
   features {}
 }
+
+resource "azurerm_user_assigned_identity" "example" {
+ resource_group_name = data.azurerm_resource_group.example.name
+  location                 = data.azurerm_resource_group.example.location
+  name                = "example"
+  
+}
