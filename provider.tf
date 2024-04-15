b871b2a7ee404242468577ed229af3dcba41d325
Kalgi Malani <67944474+Kalgi-94@users.noreply.github.com>
2024-04-15 16:54:03 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,7 +10,7 @@ terraform {
 provider "azurerm" {
   subscription_id = "3ec5e19d-8ef4-4c47-a9f2-f2ece03412ed"
   client_id       = "0f38f3b4-3bc7-440b-811d-fe754b64837d"
-  client_secret   = "DCR8Q~HX~ct6Y3QlHoTqDu.g3U.LOFzYNVfsNcXk"
+  client_secret   = "SAU8Q~QAXT5DddumP78Zu6XN8hp1o43nxfk2GcM1"
   tenant_id       = "50910f11-83a2-4159-bd1b-f0aa1521666e"
 skip_provider_registration = true
   features {}
