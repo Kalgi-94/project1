dbde1093dde56a36f4be5093091af4679616bee4
Kalgi Malani <67944474+Kalgi-94@users.noreply.github.com>
2024-04-15 15:22:35 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,7 +8,9 @@ terraform {
 }
 
 provider "azurerm" {
-  features {
-  }
+ subscription_id = 3ec5e19d-8ef4-4c47-a9f2-f2ece03412ed
+  client_id       = 0f38f3b4-3bc7-440b-811d-fe754b64837d
+  client_secret   = DCR8Q~HX~ct6Y3QlHoTqDu.g3U.LOFzYNVfsNcXk
+  tenant_id       = 50910f11-83a2-4159-bd1b-f0aa1521666e
 
 }
