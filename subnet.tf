ee3332fe640800e4feebcba8edca59c69c301ddb
Kalgi Malani <67944474+Kalgi-94@users.noreply.github.com>
2024-04-15 16:44:08 -0400

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,6 +1,1 @@
-resource "azurerm_subnet" "TFinternal" {
-  name                 = "TFinternal"
-  resource_group_name  = azurerm_resource_group.TFrg.name
-  virtual_network_name = azurerm_virtual_network.TFnetwork.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
+
