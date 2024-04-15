254f6258b1bdae661f5012db76f3016d3b999421
Kalgi Malani <67944474+Kalgi-94@users.noreply.github.com>
2024-04-15 17:53:26 -0400

    Update vmnet.tf

--- vmnet.tf
+++ vmnet.tf
@@ -1,1 +1,6 @@
-
+resource "azurerm_virtual_network" "TFnetwork" {
+  name                = "TFvmnw"
+  address_space       = ["10.0.0.0/22"]
+  location            = azurerm_resource_group.TFrg.location
+  resource_group_name = azurerm_resource_group.TFrg.name
+}
