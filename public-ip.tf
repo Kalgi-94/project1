83ef2209f957b057bc30e064d0d35d0d71b8df44
Kalgi Malani <67944474+Kalgi-94@users.noreply.github.com>
2024-04-15 17:52:13 -0400

    Update public-ip.tf

--- public-ip.tf
+++ public-ip.tf
@@ -1,1 +1,10 @@
+resource "azurerm_public_ip" "TFpublicip" {
+  name                = "TFpublicIP"
+  resource_group_name = azurerm_resource_group.TFrg.name
+  location            = azurerm_resource_group.TFrg.location
+  allocation_method   = "Dynamic"
 
+  tags = {
+    environment = "Production"
+  }
+}
