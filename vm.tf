0d014efb46e28ea42015079eb9f7fe545d5c4a98
Kalgi Malani <67944474+Kalgi-94@users.noreply.github.com>
2024-04-15 17:53:01 -0400

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -1,1 +1,24 @@
+resource "azurerm_linux_virtual_machine" "main" {
+  name                            = "TFVirtualMachine"
+  resource_group_name             = azurerm_resource_group.TFrg.name
+  location                        = "eastus"
+  size                            = "Standard_D2s_v3"
+  admin_username                  = "TFvmachine1"
+  admin_password                  = "TFvmachine1"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.TFnic.id
+  ]
 
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+}
